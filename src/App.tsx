7d1813a11a70bbd7783e7867b5169616dc499849
ZIDIKHERY MCHOMVU <101670659+LXXOIII@users.noreply.github.com>
2022-10-07 15:27:27 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import GitHub from './GitHub'
+// import UserForm from './UserForm'
+
+function App() {
+  return (
+    <>
+      {/* <UserForm /> */}
+      <GitHub />
+    </>
+  )
+}
+
+export default App
